56a572d079c2a81b6f74515485235ab4eba663d8
ERA <era@ERAs-Mac-mini.local>
2019-07-16 14:38:37 +0800

    second commit

--- phyomessageilbrary/src/main/java/com/example/phyomessage/PhyoMessage.java
+++ phyomessageilbrary/src/main/java/com/example/phyomessage/PhyoMessage.java
@@ -1,15 +1,58 @@
 package com.example.phyomessage;
 
 import android.content.Context;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.widget.TextView;
 import android.widget.Toast;
+
+import androidx.appcompat.app.AlertDialog;
+import androidx.recyclerview.widget.LinearLayoutManager;
+import androidx.recyclerview.widget.RecyclerView;
+
+import com.example.phyomessage.TitleAndList.DialogListAdapter;
+
+import java.util.List;
 
 public class PhyoMessage {
     public static void TitleAndMessage(Context context, String title, String message) {
-        Toast.makeText(context, message, Toast.LENGTH_SHORT).show();
+        AlertDialog.Builder dialog = new AlertDialog.Builder(context);
+        dialog.setTitle(title);
+        dialog.setMessage(message);
+        AlertDialog alert = dialog.create();
+        alert.show();
     }
 
     public static void MessageOnly(Context context, String message) {
-        Toast.makeText(context, message, Toast.LENGTH_SHORT).show();
+        AlertDialog.Builder dialog = new AlertDialog.Builder(context);
+        dialog.setMessage(message);
+        AlertDialog alert = dialog.create();
+        alert.show();
+    }
+    public static void showPhyoDialog(Context context, String title, String message, List<String> listItems) {
+        AlertDialog.Builder dialog = new AlertDialog.Builder(context);
+        View dialogView = LayoutInflater.from(context).inflate(R.layout.dialog_layout, null);
+        TextView dialogTitle = dialogView.findViewById(R.id.dialog_title);
+        TextView dialogMessage = dialogView.findViewById(R.id.dialog_message);
+        RecyclerView recyclerView = dialogView.findViewById(R.id.dialog_list_recycler);
+        if (title.length() > 0) {
+            dialogTitle.setText(title);
+        }else {
+            dialogTitle.setVisibility(View.GONE);
+        }
+
+        if (message.length() > 0) {
+            dialogMessage.setText(message);
+        }else {
+            dialogMessage.setVisibility(View.GONE);
+        }
+
+        recyclerView.setLayoutManager(new LinearLayoutManager(context));
+        DialogListAdapter adapter = new DialogListAdapter(context, listItems);
+        recyclerView.setAdapter(adapter);
+        dialog.setView(dialogView);
+        AlertDialog alertDialog = dialog.create();
+        alertDialog.show();
     }
 
 }
---
+++ phyomessageilbrary/src/main/java/com/example/phyomessage/TitleAndList/DialogListAdapter.java
@@ -0,0 +1,50 @@
+package com.example.phyomessage.TitleAndList;
+
+import android.content.Context;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.TextView;
+
+import androidx.annotation.NonNull;
+import androidx.recyclerview.widget.RecyclerView;
+
+import com.example.phyomessage.R;
+
+import java.util.List;
+
+public class DialogListAdapter extends RecyclerView.Adapter {
+    Context context;
+    List<String> listItems;
+    public DialogListAdapter(Context context, List<String> listItems) {
+        this.context = context;
+        this.listItems = listItems;
+    }
+
+    @NonNull
+    @Override
+    public RecyclerView.ViewHolder onCreateViewHolder(@NonNull ViewGroup parent, int viewType) {
+        return new ListItemViewHolder(LayoutInflater.from(context).inflate(R.layout.list_item, parent, false));
+    }
+
+    @Override
+    public void onBindViewHolder(@NonNull RecyclerView.ViewHolder holder, int position) {
+        if(holder instanceof ListItemViewHolder) {
+            ((ListItemViewHolder) holder).itemName.setText(listItems.get(position));
+        }
+    }
+
+    @Override
+    public int getItemCount() {
+        return listItems.size();
+    }
+
+    private class ListItemViewHolder extends RecyclerView.ViewHolder {
+
+        TextView itemName;
+        public ListItemViewHolder(@NonNull View itemView) {
+            super(itemView);
+            itemView = itemView.findViewById(R.id.list_item_name);
+        }
+    }
+}
